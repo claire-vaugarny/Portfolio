bd826f45850ff9b66eed158e86f9cec49b848769
Claire Vaugarny <claire.vaugarny@laposte.net>
2025-06-19 11:48:28 +0200

    version mobile : navbar.2

--- src/App.jsx
+++ src/App.jsx
@@ -8,7 +8,7 @@ import './App.scss'
 function App() {
   return (
     <main className="main">
-      {/* <Navbar /> */}
+      <Navbar />
       <ProfileSection />
       <CompetencesSection />
       <ProjectSection />

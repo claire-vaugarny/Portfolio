0df118a10046df0a02c171c6be8a34ec0cab3133
Claire Vaugarny <claire.vaugarny@laposte.net>
2025-06-19 11:27:49 +0200

    version mobile : profilSection

--- src/App.jsx
+++ src/App.jsx
@@ -9,7 +9,7 @@ function App() {
   return (
     <main className="main">
       {/* <Navbar /> */}
-      {/* <ProfileSection /> */}
+      <ProfileSection />
       <CompetencesSection />
       <ProjectSection />
     </main>

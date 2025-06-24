1d9d5ba960ce4c2da6944767d047f539c7458465
Claire Vaugarny <claire.vaugarny@laposte.net>
2025-06-24 18:20:33 +0200

    maj CV

--- src/components/profilSection/ButtonResume.jsx
+++ src/components/profilSection/ButtonResume.jsx
@@ -4,7 +4,7 @@ function ButtonResume() {
   return (
     <div className="resumeContainer">
       <h3>Mon CV</h3>
-      <a href="/Claire_Vaugarny_CV.pdf" download="Claire_Vaugarny_CV.pdf">
+      <a href="/Développeur web - Claire Vaugarny - CV.pdf" download="Développeur web - Claire Vaugarny - CV.pdf">
         <button id="buttonResume" aria-label="Télécharger mon CV au format PDF">
           Télécharger mon CV
         </button>
